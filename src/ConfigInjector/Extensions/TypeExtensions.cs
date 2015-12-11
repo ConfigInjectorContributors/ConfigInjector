03b181b918d11988ed55a81415a7046f2ef87d51
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-11 18:01:48 +1000

    Lifted some ThirdDrawer type extensions.

---
+++ src/ConfigInjector/Extensions/TypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConfigInjector.Extensions
+{
+    internal static class TypeExtensions
+    {
+        internal static bool IsInstantiable(this Type type)
+        {
+            if (type.IsInterface) return false;
+            if (type.IsAbstract) return false;
+            if (type.ContainsGenericParameters) return false;
+            return true;
+        }
+
+        internal static bool IsAssignableTo<TTarget>(this Type type)
+        {
+            return typeof(TTarget).IsAssignableFrom(type);
+        }
+    }
+}
--- src/ConfigInjector/SettingsConventions/SettingKeyConventions.cs
+++ src/ConfigInjector/SettingsConventions/SettingKeyConventions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ThirdDrawer.Extensions.TypeExtensionMethods;
+using ConfigInjector.Extensions;
 
 namespace ConfigInjector.SettingsConventions
 {
