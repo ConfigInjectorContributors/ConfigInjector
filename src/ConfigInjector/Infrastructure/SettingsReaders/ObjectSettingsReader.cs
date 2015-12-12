b6e8a37ae72e2d9256fdb23e13698c1b129f139c
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-12 12:46:23 +1000

    Added ObjectSettingsReader (courtesy of @droyad)

---
+++ src/ConfigInjector/Infrastructure/SettingsReaders/ObjectSettingsReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfigInjector.Infrastructure.SettingsReaders
+{
+    public class ObjectSettingsReader : IEnumeratingSettingsReader
+    {
+        private readonly Dictionary<string, string> _values;
+
+        public ObjectSettingsReader(object settingsObject)
+        {
+            var q = from p in settingsObject.GetType().GetProperties()
+                    where p.CanRead
+                    let v = "" + p.GetValue(settingsObject)
+                    where v != ""
+                    select new {Key = p.Name, Value = v};
+            _values = q.ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        public string ReadValue(string key)
+        {
+            string value;
+            return _values.TryGetValue(key, out value) ? value : null;
+        }
+
+        public IEnumerable<string> AllKeys => _values.Keys;
+    }
+}
