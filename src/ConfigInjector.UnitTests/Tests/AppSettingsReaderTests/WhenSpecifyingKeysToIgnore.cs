2b17f48f63eea81ae56c1bd53e46abfca2a2bf6c
Damian Maclennan <damian.maclennan@tattsgroup.com>
2016-09-02 13:41:34 +1000

    Added ability to ignore keys based on a predicate

---
+++ src/ConfigInjector.UnitTests/Tests/AppSettingsReaderTests/WhenSpecifyingKeysToIgnore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ConfigInjector.Infrastructure.SettingsReaders;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ConfigInjector.UnitTests.Tests.AppSettingsReaderTests
+{
+    public class WhenSpecifyingKeysToIgnore : TestFor<AppSettingsReader>
+    {
+        private IEnumerable<string> _keys;
+
+        protected override AppSettingsReader Given()
+        {
+            return new AppSettingsReader(new[]{"bar"});
+        }
+
+        protected override void When()
+        {
+            _keys = Subject.AllKeys;
+        }
+
+        [Test]
+        public void ItShouldIgnoreNamedKeys()
+        {
+            _keys.ShouldBe(new [] {"foo", "baz"});
+        }
+    }
+}
---
+++ src/ConfigInjector.UnitTests/Tests/AppSettingsReaderTests/WhenSpecifyingKeysToIgnoreViaARuleset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ConfigInjector.Infrastructure.SettingsReaders;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ConfigInjector.UnitTests.Tests.AppSettingsReaderTests
+{
+    public class WhenSpecifyingKeysToIgnoreViaARuleset : TestFor<AppSettingsReader>
+    {
+        private IEnumerable<string> _keys;
+
+        protected override AppSettingsReader Given()
+        {
+            return new AppSettingsReader(k => k.StartsWith("ba"));
+        }
+
+        protected override void When()
+        {
+            _keys = Subject.AllKeys;
+        }
+
+        [Test]
+        public void ItShouldIgnoreKeysThatMatchTheRule()
+        {
+            _keys.ShouldBe(new []{"foo"});
+        }
+    }
+}
